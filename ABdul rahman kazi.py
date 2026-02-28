856a53dca738897757bb5afbd3445a64d02273b7
hussain15102000-ui <hussain15102000@gmail.com>
2026-02-28 14:34:05 +0530

    Add files via upload

---
+++ ABdul rahman kazi.py
@@ -0,0 +1,93 @@
+#__________First problem____________
+
+#a=int(input("enter the first number"))
+#b=int(input("enter the second number"))
+#print("before swapping :a={a},b={b}")temp=a
+#a=b
+#b=temp
+#print(f"after swapping:a={a},b={b}")
+
+
+
+
+
+
+
+
+
+#__________Second problem___________
+
+#f = input("enter the eqaution :")
+#print(f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#__________Third problem_____________
+
+#base_price=500
+#gst=18*base_price
+#total_price=base_price+gst
+#print("total price =",total_price)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#__________Fourth problem_____________
+
+# print("____arithematic operators____")
+# a=int(input("Enter the first number :"))
+# b=int(input("Enter the second number :"))
+# print(f"fisrt number(integer):{a}(type:{type(a)})")
+# print(f"second number(integer):{b}(type:{type(b)})")
+# print(f"Addition:{a}+{b}={a+b}(type:{type (a+b)})")
+# print(f"Substraction:{a}-{b}={a-b}(type:{type (a-b)}")
+# print(f"Multiplication:{a}*{b}={a*b}(type:{type (a*b)}")
+# print(f"Division:{a}/{b}={a/b}(type:{type (a/b)}")
+
+# print("Addition:",a+b)
+# print("Substraction:",a-b)
+# print("Multiplication:",a*b)
+# print("Division:",a/b)
+
+
+
+
+
+
+
+
+
+
+
+
+#_________Fifth problem____________
+
+
+a = input("are you male")
+if a ="yes":
+    print("ok:)
